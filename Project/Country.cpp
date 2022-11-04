7c20dbb0eb7494ec5c50fb1d70eacb72995d640f
MatjereJ <matjere274@gmail.com>
2022-11-04 13:45:56 +0200

    Updated Country class

--- Project/Country.cpp
+++ Project/Country.cpp
@@ -1,42 +1,170 @@
+//
+// Created by JOHANES MATSEBA on 2022/10/19.
+//
+
 #include "Country.h"
 
-int Country::getHp()
-{
+#include <iostream>
+#include <string>
+#include <vector>
+
+Country::Country(BattleState* state){
+    this->battlestate=state;
+}
+
+Country::Country() {}
+
+void Country::selectWarTheatre() {
+    bool correctInput=false;
+    char x;
+
+    cout<<"Choose your most Lethal Theatre!! "<<"Is it:"<<endl;
+    cout<<"A-AirSpace"<<endl<<"B-Land"<<endl<<"C-Sea"<<endl;
+    cout<<"Enter A,B or C"<<endl;
+    cin>>x;
+
+    while (!correctInput) {
+        if (x == 'A' || x == 'a') {
+            warTheatre = new AirSpace();
+            correctInput = true;
+        } else if (x == 'B' || x == 'b') {
+            warTheatre = new Land();
+            correctInput = true;
+        } else if (x == 'C' || x == 'c') {
+            warTheatre = new Sea();
+            correctInput = true;
+        } else {
+cout<<"Invalid input. Enter A,B or C"<<endl;
+            correctInput= false;
+        }
+    }
+}
+
+Country* Country::pickOpposingCountry(vector<Country*> enemies) {
+
+    srand(time(0));
+    int random=rand()% enemies.size();
+    OpposingCountry=enemies.at(random);
+
+    return OpposingCountry;
+
+}
+
+void Country::createWarParticipants() {
+
+
+}
+
+BattleState* Country::getBattleState() {
+    return battlestate;
+}
+
+void Country::setBattleState(BattleState *s) {
+    battlestate=s;
+}
+
+int Country::getHp() {
     return hp;
 }
 
-Country *Country::getOpposingC()
-{
-    return OpposingCountry;
+void Country::attackOpposingCountry(Country *opp) {
+    battlestate->handleChange(this);
 }
 
-Country::Country(Country *C)
-{
-    OpposingCountry = C;
+
+CountryBackup* Country::createBackup() {
+    return new CountryBackup(hp,battlestate,warTheatre,warParticipants,countryObservers,OpposingCountry,win);
 }
 
-void Country::setHp(int HP)
-{
-    hp = HP;
+
+
+void Country::reinstateCountry(CountryBackup *backup) {
+    State* s=backup->state;
+    hp=s->getHP();
+    battlestate=s->getBattleState();
+    warTheatre=s->getWarTheatre();
+    warParticipants=s->getWarParticipants();
+    countryObservers=s->getCountryObservers();
+    OpposingCountry=s->getOppCountry();
+    win=s->getW();
 }
 
-list<WarParticipants *> Country::getArtillery()
+
+//Mokoena
+Country::Country(std::string name)
 {
-    return w;
+	cName = name;
 }
 
-void Country::addArtillery(WarParticipants *W)
+
+Country::~Country()
 {
-    w.push_back(W);
+	warParticipants.clear();
+	std::cout<<"Parent of "<<cName <<" deleted.\n";
 }
 
-void Country::InflictDamage(int Dmg)
+
+
+void Country::addWarParticipant(WarParticipant* warPtc)
 {
-    hp = hp - Dmg;
+	warParticipants.push_back(warPtc);
 }
 
-Country::Country()
+
+
+std::vector<CountryObserver*> Country::getCountryObservers()
 {
-    hp = 1000;
-    OpposingCountry = nullptr;
+	return countryObservers;
+}
+
+
+
+
+WarTheatre* Country::getWarTheatre()
+{
+	return warTheatre;
+}
+
+
+
+WarParticipantIterator* Country::createWarParticipantIterator()
+{
+	return new WarParticipantIterator(this);
+}
+
+
+
+std::string Country::getName()
+{
+	return cName;
+}
+
+//Areyeng Observer
+
+bool Country::add(CountryObserver* assistance){
+    countryObservers.push_back(assistance);
+    return true;
+}
+bool Country::remove(CountryObserver* assistance){
+    bool found = false;
+
+    vector<CountryObserver*>::iterator it = countryObservers.begin();
+    while ((it != countryObservers.end()) && (! found)) {
+        if (*it == assistance) {
+            found = true;
+            countryObservers.erase(it);
+        }
+        ++it;
+    }
+    return found;
+}
+void Country::notify(){
+    vector<CountryObserver*>::iterator it = countryObservers.begin();
+    for (it = countryObservers.begin(); it != countryObservers.end(); ++it){
+        (*it)->update();
+    }
+}
+
+vector<WarParticipant*> Country::getArtillery() {
+    return warParticipants;
 }

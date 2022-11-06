4d36aa1de16f5ebc3c25b0ab088dcb2e53c2875c
MurrayGibbon <murraygibbon@icloud.com>
2022-11-06 07:46:14 +0200

    tidy up structure

--- System/IndividualCountry.cpp
+++ System/IndividualCountry.cpp
@@ -25,75 +25,76 @@ IndividualCountry::IndividualCountry(string name, int level, bool sea){
     WarTransportFactory* warTransportFactory=new WarTransportFactory();
     if(level==1){
         for(int i=0; i<rand()%100+51; i++){
-            wp.push_back(soldierFactory->createMachineGunner(50));
+            wP.push_back(soldierFactory->createMachineGunner(50));
         }
         for(int i=0; i<rand()%100+51; i++){
-            wp.push_back(soldierFactory->createRifleman(60));
+            wP.push_back(soldierFactory->createRifleman(60));
         }
         for(int i=0; i<rand()%40+21; i++){
-            wp.push_back(explosiveFactory->createBomb(100));
+            wP.push_back(explosiveFactory->createBomb(100));
         }
         for(int i=0; i<rand()%40+21; i++){
-            wp.push_back(explosiveFactory->createMissile(70));
+            wP.push_back(explosiveFactory->createMissile(70));
         }
         for(int i=0; i<rand()%10+11; i++){
-            wp.push_back(warTransportFactory->createAirCraft(300));
+            wP.push_back(warTransportFactory->createAirCraft(300));
         }
         if(sea==true) {
             for (int i = 0; i < rand()%10+11; i++) {
-                wp.push_back(warTransportFactory->createWarShip(250));
+                wP.push_back(warTransportFactory->createWarShip(250));
             }
         }
     }else if(level==2){
         for(int i=0; i<rand()%150+51; i++){
-            wp.push_back(soldierFactory->createMachineGunner(50));
+            wP.push_back(soldierFactory->createMachineGunner(50));
         }
         for(int i=0; i<rand()%150+51; i++){
-            wp.push_back(soldierFactory->createRifleman(60));
+            wP.push_back(soldierFactory->createRifleman(60));
         }
         for(int i=0; i<rand()%40+31; i++){
-            wp.push_back(explosiveFactory->createBomb(100));
+            wP.push_back(explosiveFactory->createBomb(100));
         }
         for(int i=0; i<rand()%40+31; i++){
-            wp.push_back(explosiveFactory->createMissile(70));
+            wP.push_back(explosiveFactory->createMissile(70));
         }
         for(int i=0; i<rand()%10+16; i++){
-            wp.push_back(warTransportFactory->createAirCraft(300));
+            wP.push_back(warTransportFactory->createAirCraft(300));
         }
         if(sea==true) {
             for (int i = 0; i < rand() %%10+16; i++) {
-                wp.push_back(warTransportFactory->createWarShip(250));
+                wP.push_back(warTransportFactory->createWarShip(250));
             }
         }
     }else if(level==3){
         for(int i=0; i<rand()%200+51; i++){
-            wp.push_back(soldierFactory->createMachineGunner(50));
+            wP.push_back(soldierFactory->createMachineGunner(50));
         }
         for(int i=0; i<rand()%200+51; i++){
-            wp.push_back(soldierFactory->createRifleman(60));
+            wP.push_back(soldierFactory->createRifleman(60));
         }
         for(int i=0; i<rand()%40+41; i++){
-            wp.push_back(explosiveFactory->createBomb(100));
+            wP.push_back(explosiveFactory->createBomb(100));
         }
         for(int i=0; i<rand()%40+41; i++){
-            wp.push_back(explosiveFactory->createMissile(70));
+            wP.push_back(explosiveFactory->createMissile(70));
         }
         for(int i=0; i<rand()%10+21; i++){
-            wp.push_back(warTransportFactory->createAirCraft(300));
+            wP.push_back(warTransportFactory->createAirCraft(300));
         }
         if(sea==true) {
             for (int i = 0; i < rand() %%10+21; i++) {
-                wp.push_back(warTransportFactory->createWarShip(250));
+                wP.push_back(warTransportFactory->createWarShip(250));
             }
         }
     }
->>>>>>> Stashed changes
 }
+
 bool IndividualCountry::add(CountryObserver* assistance){
     countryObservers.push_back(assistance); 
     cout<<"Added Country Observer"<<endl;
     return true;
 }
+
 bool IndividualCountry::remove(CountryObserver* assistance){
   bool found = false;
 
@@ -107,31 +108,39 @@ bool IndividualCountry::remove(CountryObserver* assistance){
   }
   return found;
 }
+
 void IndividualCountry::notify(){
     vector<CountryObserver*>::iterator it = countryObservers.begin();
   for (it = countryObservers.begin(); it != countryObservers.end(); ++it){
     (*it)->update();
   }
 }
+
 string IndividualCountry::getName(){
     return this->name;
 }
+
 int IndividualCountry::getWeaponHP(){
     return this->weaponHP;
 }
+
 int IndividualCountry::getSoldierHP(){
     return this->soldierHP;
 }
+
 int IndividualCountry::getTransport(){
     return this->transport;
 }
+
 vector<IndividualCountry*> IndividualCountry::getAlliance(){
     return this->alliance;
 }
+
 void IndividualCountry::setAlliance(vector<IndividualCountry*> alliance){
   this->alliance= alliance;
 
 }
+
 void IndividualCountry::checkHp(){
     notify();
 }
@@ -139,6 +148,7 @@ void IndividualCountry::checkHp(){
 int IndividualCountry::getInitialHP(){
       return this->initialHP;
 }
+
 vector<WarParticipant*> IndividualCountry::getWarParticipants(){
     return this->wP;
 }
